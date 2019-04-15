0ceaab7eae2b8615dada304777c00077a078709d
FinalJTth <FinalJTth_31@hotmail.com>
2019-04-16 04:59:26 +0700

    New package classification and backing-up before massive renovation
    - Change package name from display to game.display
    - Change package name from game to game.engine
    - Change Game.java to Game_OLD.java as a backup and renovate Game.java
    to be more convenient to be called and used

--- src/game/engine/Launcher.java
+++ src/game/engine/Launcher.java
@@ -1,12 +1,12 @@
-package game;
+package game.engine;
 
-import display.Display;
+import game.display.Display;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game game = new Game("Title", 300, 300);
+		Game_OLD game = new Game_OLD("Title", 300, 300);
 		game.start();
 	}
 
