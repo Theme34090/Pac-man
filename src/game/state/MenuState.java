47d0ee1f0a944eb4277f7efe279fe6a41e384daf
Teeradham <5batman400@gmail.com>
2019-04-16 22:54:24 +0700

    add gameState

---
+++ src/game/state/MenuState.java
@@ -0,0 +1,26 @@
+package game.state;
+
+import java.awt.Graphics;
+
+import game.Game;
+
+public class MenuState extends State {
+
+	public MenuState(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
